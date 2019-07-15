85620e896e6ad4d2eab8c130ed04670e97a4e8a4
Ella Butcher <ella@Ellas-MacBook-Air.local>
2019-07-15 13:03:04 +0100

    loaded html from hockey site

--- Netball.py
+++ Netball.py
@@ -1,2 +1,17 @@
+#!/usr/bin/env python3
+
+import requests
+
+
+def get_hockey_results_page():
+    page = requests.get("https://www.nwc2019.co.uk/competition-hub")
+    return page
+
+
+def main():
+    hockeypage = get_hockey_results_page()
+    print(hockeypage.content)
+
 if __name__ == "__main__":
-    print("Hello")
+    main()
+   
