bdedc4a988c1aa10b489659090b80a9fc524ebd7
Ella Butcher <ella@Ellas-MacBook-Air.local>
2019-07-15 15:31:29 +0100

    start extracting team names from results

--- Netball.py
+++ Netball.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 import requests
+from bs4 import BeautifulSoup
 
 
-def get_hockey_results_page():
+def print_website(html_content):
+    soup = BeautifulSoup(html_content,features="html.parser")
+    teambox_list = soup.select('#option2 .teambox')
+    for teambox in teambox_list:
+        team1 = teambox.select(".team1")
+        print(team1[0])
+
+
+def get_netball_results_page():
     page = requests.get("https://www.nwc2019.co.uk/competition-hub")
     return page
 
 
 def main():
-    hockeypage = get_hockey_results_page()
-    print(hockeypage.content)
+    netballpage = get_netball_results_page()
+    print_website(netballpage.content)
+
 
 if __name__ == "__main__":
     main()
