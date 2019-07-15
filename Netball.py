eea092ec894e84eca39ff2c1e179473555752c5b
Ella Butcher <ella@Ellas-MacBook-Air.local>
2019-07-15 16:25:59 +0100

    added results scores

--- Netball.py
+++ Netball.py
@@ -4,12 +4,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def print_website(html_content):
+def print_gather_results(html_content):
     soup = BeautifulSoup(html_content,features="html.parser")
     teambox_list = soup.select('#option2 .teambox')
+    results_list = []
     for teambox in teambox_list:
+        result = []
         team1 = teambox.select(".team1")
-        print(team1[0])
+        team2 = teambox.select(".team2")
+        scores = teambox.select(".status")
+        team1_score = scores[0].get_text()
+        team2_score = scores[0].get_text()
+        result.append(team1[0].get_text())
+        result.append(team1_score)
+        result.append(team2[0].get_text())
+        result.append(team2_score)
+        results_list.append(result)
+    
+
+
+    print(results_list)
 
 
 def get_netball_results_page():
@@ -19,7 +33,7 @@ def get_netball_results_page():
 
 def main():
     netballpage = get_netball_results_page()
-    print_website(netballpage.content)
+    print_gather_results(netballpage.content)
 
 
 if __name__ == "__main__":
