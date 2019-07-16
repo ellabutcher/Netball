1e640e162103c8a95adb479386f7370a12ec24fc
Ella Butcher <ella@Ellas-MacBook-Air.local>
2019-07-16 12:24:18 +0100

    Extract results from 2015 sky results page

--- Netball.py
+++ Netball.py
@@ -14,7 +14,7 @@ def gather_results(html_content):
         team2 = teambox.select(".team2")
         scores = teambox.select(".status")
         team1_score = scores[0].get_text()
-        team2_score = scores[0].get_text()
+        team2_score = scores[1].get_text()
         result.append(team1[0].get_text())
         result.append(team1_score)
         result.append(team2[0].get_text())
@@ -24,12 +24,37 @@ def gather_results(html_content):
 
 
 def print_results(team_name, results_list):
-    for team_name in results_list:
-        if team1 or team2 is "England":
-            print(results_list)
-        else:
-            print_results
+    for result in results_list:
+        team1  = result[0]
+        team2 = result[2]
+        if team1 == team_name or team2 == team_name:
+            print(result)
+        
 
+def gather_historic_results(team_name, years):
+    results = []
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    for year in range(2019, 2019 - years, -1):
+        for month in months:
+            url = "https://www.skysports.com/netball/results/" +month +"-" + str(year)
+            #scrape_sky_results(url, results)
+    scrape_sky_results("https://www.skysports.com/netball/results/August-2015", results)
+    print (results)
+
+def scrape_sky_results(url, results_list):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,features="html.parser")
+    match_results = soup.select('.matches__list-item')
+    for match in match_results:
+        result = []
+        scores = match.select(".matches__teamscores-side")
+        teams = match.select(".swap-text__target")
+        print (teams[0].text)
+        result.append(teams[0].text)
+        result.append(int(scores[0].text))
+        result.append(teams[1].text)
+        result.append(int(scores[1].text))
+        results_list.append(result)
 
 
 def get_netball_results_page():
@@ -41,6 +66,8 @@ def main():
     netballpage = get_netball_results_page()
     results = gather_results(netballpage.content)
     print_results("England", results)
+    print_results("Scotland", results)
+    gather_historic_results("England", 2)
     
 
 
